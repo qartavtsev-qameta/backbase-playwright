4507c6ff481db6526d1ac2180deecdc5442d9472
Artem Eroshenko <eroshenkoam@gmail.com>
2025-05-07 13:10:29 +0300

    add manual test

--- test/account.test.ts
+++ test/account.test.ts
@@ -1,8 +1,10 @@
 const { test } = require("@playwright/test");
 const allure = require("allure-js-commons");
 
+
 test("Verify confirmation message after updating profile", async () => {
   await allure.epic("Account");
+  await allure.label("AS_MANUAL", "true");
   await allure.feature("Account Settings");
   await allure.story("Update profile information");
 
@@ -21,4 +23,15 @@ test("Verify email preferences can be customized", async () => {
   await allure.step("When the user updates their email notification settings", async () => { });
   await allure.step("Then the email preferences should be saved", async () => { });
   await allure.step("And the user should receive emails according to the updated preferences", async () => { });
+});
+
+test("Verify user can log out from a specific device @allure.id:10226 @Positive", async () => {
+  await allure.epic("Account");
+  await allure.feature("Security Settings");
+  await allure.story("Device management");
+
+  await allure.step("Given the user is viewing their logged-in devices", async () => { });
+  await allure.step("When the user selects a device and clicks Log Out", async () => { });
+  await allure.step("Then the device should be logged out", async () => { });
+  await allure.step("And a confirmation message should be shown", async () => { });
 });
