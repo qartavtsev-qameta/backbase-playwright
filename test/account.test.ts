1f12511d29af804e8be8f3b9ac24b6e8d42a6e02
Artem Eroshenko <eroshenkoam@gmail.com>
2025-05-07 12:47:18 +0300

    fix build

--- test/account.test.ts
+++ test/account.test.ts
@@ -1,24 +1,24 @@
 const { test } = require("@playwright/test");
 const allure = require("allure-js-commons");
 
-test("Verify confirmation message after updating profile @allure.id:10222", async() => {
+test("Verify confirmation message after updating profile @allure.id:10222", async () => {
   await allure.epic("Account");
   await allure.feature("Account Settings");
   await allure.story("Update profile information");
 
-  await allure.step("Given the user has updated their profile details", async () => {});
-  await allure.step("When the user clicks Save Changes", async () => {});
-  await allure.step("Then a confirmation message should be displayed", async () => {});
-  await allure.step("And the updated profile information should be shown", async () => {});
+  await allure.step("Given the user has updated their profile details", async () => { });
+  await allure.step("When the user clicks Save Changes", async () => { });
+  await allure.step("Then a confirmation message should be displayed", async () => { });
+  await allure.step("And the updated profile information should be shown", async () => { });
 });
 
-test("Verify email preferences can be customized", async() => {
-    await allure.epic("Account");
-    await allure.feature("Account Settings");
-    await allure.story("Change notification preferences");
+test("Verify email preferences can be customized", async () => {
+  await allure.epic("Account");
+  await allure.feature("Account Settings");
+  await allure.story("Change notification preferences");
 
-    await allure.step("Given the user is on the email preferences page", async () => {});
-    await allure.step("When the user updates their email notification settings", async () => {});
-    await allure.step("Then the email preferences should be saved", async () => {});
-    await allure.step("And the user should receive emails according to the updated preferences", async () => {});
+  await allure.step("Given the user is on the email preferences page", async () => { });
+  await allure.step("When the user updates their email notification settings", async () => { });
+  await allure.step("Then the email preferences should be saved", async () => { });
+  await allure.step("And the user should receive emails according to the updated preferences", async () => { });
 });
