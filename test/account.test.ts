2824611061f9f527765b06bf3849e36fb15a63cc
Artem Eroshenko <eroshenkoam@gmail.com>
2025-05-07 12:42:59 +0300

    add test cases

--- test/account.test.ts
+++ test/account.test.ts
@@ -1,15 +1,24 @@
-import { test } from "@playwright/test";
-import * as allure from "allure-js-commons";
+const { test } = require("@playwright/test");
+const allure = require("allure-js-commons");
 
-test("sample test", async () => {
-  await allure.links(...[{ url: "https://example.org"}]);
-  await allure.owner("John Doe");
-  await allure.issue("JIRA-2", "https://example.org");
-  await allure.step("step 1", async () => {
-    await allure.step("step 1.2", async () => {
-      await allure.attachment("text attachment", "some data", "text/plain");
-    });
-  });
-  await allure.step("step 2", async () => {
-  });
+test("Verify confirmation message after updating profile @allure.id:10222", async() => {
+  await allure.epic("Account");
+  await allure.feature("Account Settings");
+  await allure.story("Update profile information");
+
+  await allure.step("Given the user has updated their profile details", async () => {});
+  await allure.step("When the user clicks Save Changes", async () => {});
+  await allure.step("Then a confirmation message should be displayed", async () => {});
+  await allure.step("And the updated profile information should be shown", async () => {});
+});
+
+test("Verify email preferences can be customized", async() => {
+    await allure.epic("Account");
+    await allure.feature("Account Settings");
+    await allure.story("Change notification preferences");
+
+    await allure.step("Given the user is on the email preferences page", async () => {});
+    await allure.step("When the user updates their email notification settings", async () => {});
+    await allure.step("Then the email preferences should be saved", async () => {});
+    await allure.step("And the user should receive emails according to the updated preferences", async () => {});
 });
